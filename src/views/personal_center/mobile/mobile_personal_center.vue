41a4f0cef14ba5c0164cea47db8e0cbd6e06fa4b
Delight.L&M <oncwnuOJmSsBlKUJdHd3mqW49qYw@git.weixin.qq.com>
2023-03-01 16:55:55 +0800

    双端我的活动列表展示问题解决-1

--- src/views/personal_center/mobile/mobile_personal_center.vue
+++ src/views/personal_center/mobile/mobile_personal_center.vue
@@ -22,12 +22,12 @@
                   <span>个人财富 </span>
                 </el-col>
                 <el-col  :span="9">
-                  <span style="color: #ff697b;font-weight: bold;font-size: medium;margin-left: 5px ">11111.11</span>
-                  <span>诸葛贝</span>
+                  <span style="color: #ff697b;font-weight: bold;font-size: medium;margin-left: 5px ">{{numFilter(myActivity.coin,2)}}</span>
+                  <span> 诸葛贝</span>
                 </el-col>
                 <el-col :span="9">
                   <span style="margin-left: 5px">交易次数 </span>
-                  <span style="color: #2e2e2e;font-weight: bold;font-size: medium; ">123243</span>
+                  <span style="color: #2e2e2e;font-weight: bold;font-size: medium ">{{myActivity.trade_times}}</span>
                 </el-col>
               </el-row>
               <el-row style="margin-top: 10px">
